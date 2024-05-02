8a4f6db80d1d17ab49895985f2e4841638fb3d35
Abrhm277 <canales231@gmail.com>
2024-05-02 15:43:11 +0200

    disenofacturacompra

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+void disenofacturadecompra () {
+     printf("disenofacturadecompra");
+
+}
+
 int main() {
   printf("Hola, Mundo!");
+  disenofacturadecompra;
   return 0;
 }

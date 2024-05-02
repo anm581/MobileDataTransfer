ee8d6179c5017e32d96afc1c8d41b35cc7bcb68b
anm581 <angel2hbc74@gmail.com>
2024-05-02 17:19:33 +0200

    arreglo de main.c

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void disenolobby() {
+  printf("Diseño de lobby día 1");
+  printf("Diseño de lobby día 2");
+  printf("Diseño de lobby día 3");
+  return 0;
+}
+
+void  ter_con_cuenta(){
+ printf("Terminos y Condiciones de cuenta");
+ printf("Terminos y Condiciones de cuenta dia 2");
+ printf("Terminos y Condiciones de cuenta dia 3");
+}
+
 void PopUpCompra(){
   printf("Programación del Pop Up de confirmación de la compra");
 }
@@ -8,13 +21,12 @@ void PopUpVenta(){
   printf("Programación del Pop Up de confirmación de la venta");
 }
 
-void diseñosignin(){
+void disenosignin(){
   printf("Diseño Sign In Día 1");
   printf("Diseño Sign In Día 2");
 }
 
 void disenofacturadecompra () {
-<<<<<<< HEAD
      printf("disenofacturadecompra");
      printf("disenofacturadecompradia2");
      printf("disenofacturadecompradia3");
@@ -27,7 +39,6 @@ void Disenofacturacompra(){
 void disenofacturadeventa(){
    printf("disenofacturadeventa");
 
-=======
   printf("disenofacturadecompra");
   printf("disenofacturadecompradia2");
   printf("disenofacturadecompradia3");
@@ -41,41 +52,17 @@ void fun_lobby(){
   printf("Funcionamiento de Lobby día 4");
   printf("Funcionamiento de Lobby día 5");
   printf("Funcionamiento de Lobby día 6");
->>>>>>> funcionamiento_lobby
 }
 
 int main() {
   printf("Hola, Mundo!");
   PopUpCompra();
   PopUpVenta();
-  diseñolobby();
-  diseñosignin();
+  disenolobby();
+  disenosignin();
   disenofacturadecompra();
-  diseñolobby();
-<<<<<<< HEAD
   ter_con_cuenta();
   Disenofacturacompra();
   disenofacturadeventa();
-=======
->>>>>>> funcionamiento_lobby
   return 0;
 }
-
-void diseñolobby() {
-  printf("Diseño de lobby día 1");
-  printf("Diseño de lobby día 2");
-  printf("Diseño de lobby día 3");
-<<<<<<< HEAD
-  return 0;
-}
-
-void  ter_con_cuenta(){
- printf("Terminos y Condiciones de cuenta");
- printf("Terminos y Condiciones de cuenta dia 2");
- printf("Terminos y Condiciones de cuenta dia 3");
- return(0);
-}
-=======
-  return(0);
-}
->>>>>>> funcionamiento_lobby

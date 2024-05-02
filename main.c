8ccbe6d31df192fd4d49967eb60d568bd1fcc4c7
Cristian <clb272@alumnos.unican.es>
2024-05-02 15:28:54 +0200

    popupcompra

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+void PopUpCompra(){
+  printf("Programación del Pop Up de confirmación de la compra");
+}
+
 int main() {
   printf("Hola, Mundo!");
+  PopUpCompra();
   return 0;
 }

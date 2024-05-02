f32c59eff22d57249f5d087ed1d03364658fba57
Cristian <clb272@alumnos.unican.es>
2024-05-02 15:52:51 +0200

    popupventa

--- main.c
+++ main.c
@@ -4,8 +4,13 @@ void PopUpCompra(){
   printf("Programación del Pop Up de confirmación de la compra");
 }
 
+void PopUpVenta(){
+  printf("Programación del Pop Up de confirmación de la venta");
+}
+
 int main() {
   printf("Hola, Mundo!");
   PopUpCompra();
+  PopUpVenta();
   return 0;
 }

a29408d8d6ac0ab895cdeff8919f5bef66e61c95
anm581 <angel2hbc74@gmail.com>
2024-05-02 16:51:18 +0200

    Funcionamiento de Lobby día 5

--- main.c
+++ main.c
@@ -4,16 +4,44 @@ void PopUpCompra(){
   printf("Programación del Pop Up de confirmación de la compra");
 }
 
+void PopUpVenta(){
+  printf("Programación del Pop Up de confirmación de la venta");
+}
+
+void diseñosignin(){
+  printf("Diseño Sign In Día 1");
+  printf("Diseño Sign In Día 2");
+}
+
+void disenofacturadecompra () {
+  printf("disenofacturadecompra");
+  printf("disenofacturadecompradia2");
+  printf("disenofacturadecompradia3");
+  printf("disenofacturadecompradia4");
+}
+
 void fun_lobby(){
-  printf("Funcionamiento de lobby día 1");
-  printf("Funcionamiento de lobby día 2");
-  printf("Funcionamiento de lobby día 3");
-  printf("Funcionamiento de lobby día 4");
+  printf("Funcionamiento de Lobby día 1");
+  printf("Funcionamiento de Lobby día 2");
+  printf("Funcionamiento de Lobby día 3");
+  printf("Funcionamiento de Lobby día 4");
+  printf("Funcionamiento de Lobby día 5");
 }
 
 int main() {
   printf("Hola, Mundo!");
   PopUpCompra();
-  fun_lobby();
+  PopUpVenta();
+  diseñolobby();
+  diseñosignin();
+  disenofacturadecompra();
+  diseñolobby();
   return 0;
+}
+
+void diseñolobby() {
+  printf("Diseño de lobby día 1");
+  printf("Diseño de lobby día 2");
+  printf("Diseño de lobby día 3");
+  return(0);
 }

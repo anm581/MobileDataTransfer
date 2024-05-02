8e75491ffca2f0f78901612d3d9ae6770821d262
anm581 <angel2hbc74@gmail.com>
2024-05-02 15:54:26 +0200

    Funcionamiento de lobby

--- main.c
+++ main.c
@@ -4,8 +4,13 @@ void PopUpCompra(){
   printf("Programación del Pop Up de confirmación de la compra");
 }
 
+void fun_lobby(){
+  printf("Funcionamiento de lobby");
+}
+
 int main() {
   printf("Hola, Mundo!");
   PopUpCompra();
+  fun_lobby();
   return 0;
 }
